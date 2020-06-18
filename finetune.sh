1736a305f2d293387ffda4850a7f5fb9e143e1db
ChesterDu <40878339+ChesterDu@users.noreply.github.com>
2020-06-18 23:30:31 +0800

    fasdfadfa

--- finetune.sh
+++ finetune.sh
@@ -1,6 +1,6 @@
 cd /opt/repo/gpt2_finetune/
 pip install --upgrade pip
-pip install -r ../requirements.txt
+pip install -r requirements.txt
 python download_model.py 124M
 cd src
 python main.py --is_train 1 --cond 0 --model_dir models/124M --batch_size 50 --data_name kg --data_dir data
