997a78429fd6d8ef5ffe30ed67e0ef2bc0392ca1
ChesterDu <40878339+ChesterDu@users.noreply.github.com>
2020-06-18 23:33:09 +0800

    fasdfadfadfa

--- finetune.sh
+++ finetune.sh
@@ -3,5 +3,5 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python download_model.py 124M
 cd src
-python main.py --is_train 1 --cond 0 --model_dir models/124M --batch_size 50 --data_name kg --data_dir data
-python main.py --is_train 0 --cond 0 --model_dir models/124M --batch_size 20 --data_name kg --data_dir data
+python main.py --is_train 1 --cond 0 --model_dir ../models/124M --batch_size 50 --data_name kg --data_dir ../data
+python main.py --is_train 0 --cond 0 --model_dir ../models/124M --batch_size 20 --data_name kg --data_dir ../data
