6a8c3c11e7365111a9d5e2c08dbd32aedf902210
ChesterDu <40878339+ChesterDu@users.noreply.github.com>
2020-06-19 11:33:00 +0800

    dfaff

--- finetune.sh
+++ finetune.sh
@@ -3,11 +3,8 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python download_model.py 124M
 # cd src
-python src/main.py --is_train 1 \ 
-                   --cond 0 \
-                   --model_dir models/124M \
-                   --batch_size 50 \
-                   --data_name kg \
-                   --data_dir data \
+# python src/main.py --model_dir "./models/124M" \
+#                    --data_name "./kg" \
+#                    --data_dir "./data" \
 
 # python src/main.py --is_train 0 \ --cond 0 \--model_dir models/124M \--batch_size 50 \--data_name kg \--data_dir data
