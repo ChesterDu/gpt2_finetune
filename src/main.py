cffbde03503879a5c6f28f12741d20a0c2baee97
ChesterDu <40878339+ChesterDu@users.noreply.github.com>
2020-06-19 14:38:09 +0800

    randone

--- src/main.py
+++ src/main.py
@@ -249,7 +249,7 @@ with tf.Session(config=config) as sess:
         while True:
             random.shuffle(data_train)
             start_time = time.time()
-            print(1)
+            print("hejkrhqwlekhrldiujfhalkjsdhfalkjsdhfalkjsdhfaklsjdfhalkdfhakljdfhalkjdhflkajdhf")
             loss = train(sess, data_train, is_train=True)
             counter += 1
             if loss > max(pre_losses):  # Learning rate decay
